2d37599da0f714072e99846d89a72bab943374d0
seungwoo <lirtha@naver.com>
2024-06-13 13:51:56 +0900

    [feature] page 30 : 3자리 아닌 경우 처리 기능 개발

--- game.py
+++ game.py
@@ -1,4 +1,7 @@
 class Game:
     def guess(self, guess_number):
         if guess_number is None:
-            raise TypeError
+            raise TypeError()
+
+        if len(guess_number) != 3:
+            raise TypeError()
--- test_game.py
+++ test_game.py
@@ -12,3 +12,7 @@ class TestGame(TestCase):
     def test_exception_when_input_is_none(self):
         with self.assertRaises(TypeError):
             self.game.guess(None)
+
+    def test_exception_when_input_length_unmatched(self):
+        with self.assertRaises(TypeError):
+            self.game.guess('12')
