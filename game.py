88483292a0262d30657db2a86ee5a9674079210e
seungwoo <lirtha@naver.com>
2024-06-13 14:02:33 +0900

    [refactoring] page 43 : 코드 리팩토링

--- game.py
+++ game.py
@@ -1,16 +1,19 @@
 class Game:
-    def guess(self, guess_number):
+    def is_duplicated_number(self, guess_number):
+        return guess_number[0] != guess_number[1] or \
+            guess_number[1] != guess_number[2] or \
+            guess_number[0] != guess_number[2]
+
+    def assert_illegal_value(self, guess_number):
         if guess_number is None:
             raise TypeError()
-
         if len(guess_number) != 3:
             raise TypeError()
-
         for number in guess_number:
             if not ord('0') <= ord(number) <= ord('9'):
                 raise TypeError()
-
-        if guess_number[0] != guess_number[1] or \
-            guess_number[1] != guess_number[2] or \
-            guess_number[0] != guess_number[2]:
+        if self.is_duplicated_number(guess_number):
             raise TypeError()
+
+    def guess(self, guess_number):
+        self.assert_illegal_value(guess_number)
